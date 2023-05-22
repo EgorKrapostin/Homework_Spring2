df4099ab30186f7212bd3d6b8914854f19625b51
ПК <ekrapostin@gmail.com>
2023-05-22 20:26:11 +0300

    Initial commit

---
+++ src/main/java/ru/skypro/lessons/springboot/homework_spring2/HomeworkSpring2Application.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.homework_spring2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkSpring2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HomeworkSpring2Application.class, args);
+    }
+
+}
---
+++ src/test/java/ru/skypro/lessons/springboot/homework_spring2/HomeworkSpring2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.skypro.lessons.springboot.homework_spring2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeworkSpring2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
